a3bb774989e2931398f6b2c11b11bbf70492c766
fevvelasquez <fevvelasquez@gmail.com>
2021-05-12 02:32:30 -0500

    Practice 14-3. Simulate Concurrent Callers.
    LESSON 14. CONCURRENCY AND MULTITHREADING.
    
    fevvelasquez 2021, coding practices from
    Java SE 11 Programming Complete Course at Oracle University.
    
    Requirement:
    	1. Write code that simulates several concurrent callers
    	   that share a single instance of the ProductManager.
    	*. Each concurrent client generate a unique client id,
    	   a random product id, select random locale and invoke
    	   getDiscounts, reviewProduct and printProductReport operations.
    	*. Each client also  produces a log of operations.
    	2. Create an executor service to launch these concurrent callers.
    
    Retrieve action logs produced by each concurrent client and
    print these logs to the console.Printing logs to the console is a sequential activity,
    this is why the concurrent client should not try to print directly to the console.
    
    Changes to be committed:
    	new file:   csv/product1103.csv
    	new file:   csv/product1104.csv
    	new file:   csv/product1105.csv
    	new file:   csv/product1106.csv
    	new file:   csv/product1107.csv
    	new file:   csv/product1108.csv
    	new file:   csv/product1109.csv
    	modified:   src/me/fevvelasquez/pm/app/Shop.java

--- src/me/fevvelasquez/pm/app/Shop.java
+++ src/me/fevvelasquez/pm/app/Shop.java
@@ -18,6 +18,18 @@ package me.fevvelasquez.pm.app;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.util.Comparator;
+import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.logging.Logger;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 import me.fevvelasquez.pm.data.Product;
 import me.fevvelasquez.pm.data.ProductManager;
 import me.fevvelasquez.pm.data.Rating;
@@ -25,7 +37,7 @@ import me.fevvelasquez.pm.data.Rating;
 /**
  * {@code Shop} class represents an application that manages Products.
  * 
- * @version 0.14.1. Redesign Product Manager as a Singleton.
+ * @version 0.14.3. Simulate Concurrent Callers.
  * @author oracle GNU GPL / fevvelasquez
  */
 public class Shop {
@@ -34,8 +46,55 @@ public class Shop {
 	 * @param args No arguments needed.
 	 */
 	public static void main(String[] args) {
-		// Create products factory using LOCALE
+		//
 		var pm = ProductManager.getInstance();
+		AtomicInteger clientCount = new AtomicInteger(0);
+		// ----------------------------------------------------------------------
+
+		// client body
+		Callable<String> client = () -> {
+			int productId = ThreadLocalRandom.current().nextInt(9) + 1101;
+			String clientId = "Client" + clientCount.incrementAndGet();
+			String threadName = Thread.currentThread().getName();
+			String languageTag = ProductManager.getSupportedLocales().stream()
+					.skip(ThreadLocalRandom.current().nextInt(5)).findFirst().get();
+
+			String ID = clientId + " " + threadName;
+			StringBuilder log = new StringBuilder();
+
+			log.append("\n");
+			log.append(ID + "\n-\tstart of log\t-\n");
+			log.append(pm.getDiscounts(languageTag).entrySet().stream()
+					.map(entry -> entry.getKey() + "\t" + entry.getValue()).collect(Collectors.joining("\n")));
+			Product product = pm.reviewProduct(productId, Rating.FOUR_STARS, "Yet Another Review.");
+			log.append((product != null) ? "\nProduct" + productId + " reviewed\n"
+					: "\nProduct" + productId + " not reviewed\n");
+			pm.printProductReport(productId, languageTag, clientId);
+			log.append(clientId + " generated report for" + productId + "product");
+			log.append(ID + "\n-\tend of log\t-\n");
+
+			return log.toString();
+		};
+		// ----------------------------------------------------------------------
+
+		//
+		List<Callable<String>> clientsList = Stream.generate(() -> client).limit(5).collect(Collectors.toList());
+		ExecutorService executorService = Executors.newFixedThreadPool(3);
+
+		try {
+			List<Future<String>> results = executorService.invokeAll(clientsList);
+			executorService.shutdown();
+
+			results.stream().forEach(result -> {
+				try {
+					System.out.println(result.get());
+				} catch (InterruptedException | ExecutionException e) {
+					Logger.getLogger(Shop.class.getName()).severe("Error retrieveing client log" + e.getMessage());
+				}
+			});
+		} catch (InterruptedException e) {
+			Logger.getLogger(Shop.class.getName()).severe("Error invoking clients" + e.getMessage());
+		}
 		// ----------------------------------------------------------------------
 
 		// From files:
@@ -64,7 +123,7 @@ public class Shop {
 		// Comparator<Product> ratingAsc = (p1, p2) -> p1.getRating().ordinal() -
 		// p2.getRating().ordinal();
 		Comparator<Product> nameLengthAsc = (p1, p2) -> p1.getName().length() - p2.getName().length();
-		pm.printProducts(p -> true, nameLengthAsc,"en-GB");
+		pm.printProducts(p -> true, nameLengthAsc, "en-GB");
 		// ----------------------------------------------------------------------
 
 	}
